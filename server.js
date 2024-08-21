2e764da213f0f9f8fb3701811f46e92be16446ab
Ashish Mehra <119057692+ashish280601@users.noreply.github.com>
2024-08-21 06:30:52 +0000

    insitallize my server project and created a basic server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).json({
+    res.status(200).json({
     message: "Welcome to task manager application",
     status: true,
   })
