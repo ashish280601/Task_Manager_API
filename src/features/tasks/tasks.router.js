bc7a10dc591dc74516699787af5ead0b3b33ef22
unknown <asmehra0628@gmail.com>
2024-08-22 01:59:31 +0530

    task

--- src/features/tasks/tasks.router.js
+++ src/features/tasks/tasks.router.js
@@ -6,19 +6,19 @@ import jwtAuth from "../../middleware/jwt.middleware.js";
 const taskRouter = express.Router();
 const taskController = new TaskController();
 
-taskRouter.get('all-task', jwtAuth, (req, res) => {
+taskRouter.get('/all-data', jwtAuth, (req, res) => {
     taskController.getTasks(req, res);
 })
 
-taskRouter.post('add', jwtAuth, (req, res) => {
+taskRouter.post('/add', jwtAuth, (req, res) => {
     taskController.addTask(req,res);
 })
 
-taskRouter.put('update', jwtAuth, (req, res) => {
+taskRouter.put('/update', jwtAuth, (req, res) => {
     taskController.updateTask(req, res);
 })
 
-taskRouter.delete('delete', jwtAuth, (req, res) => {
+taskRouter.delete('/delete', jwtAuth, (req, res) => {
     taskController.deleteTask(req, res);
 })
 
