6e853a57984c05de65aa024c9c32afb907aa1407
unknown <asmehra0628@gmail.com>
2024-08-25 11:26:16 +0530

    commit

--- src/config/aws.js
+++ src/config/aws.js
@@ -1,8 +1,5 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-console.log("aws regions", process.env.AWS_REGION);
-console.log("aws access key", process.env.AWS_ACCESS_KEY_ID);
-console.log("aws secret key", process.env.AWS_SECRET_ACCESS_KEY);
 
 // Configure AWS S3 client with access key, secret key, and region
 const s3 = new S3Client({
